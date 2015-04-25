793d9a62fd93a507339da2ce31408e3d900b93b8
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2015-04-25 09:52:41 -0400

    Finished Panel

--- KeynotePanel.java
+++ KeynotePanel.java
@@ -15,7 +15,7 @@ public class KeynotePanel extends JPanel {
     
     //START Quote Panel 
     JPanel quotePanel = new JPanel();
-    quotePanel.setLayout(new FlowLayout());
+    quotePanel.setLayout(new BorderLayout());
     c.fill        = GridBagConstraints.HORIZONTAL;
     c.gridx       =  0;
     c.gridy       =  0;
@@ -24,30 +24,35 @@ public class KeynotePanel extends JPanel {
     c.ipady       = 30;
     add(quotePanel, c);
     
-    labelQuoteStart = new JLabel(String.format("\"%s\" ", KeynoteWriter.getQuote()));
-    //labelQuoteStart = new JLabel("\"Quote\" ");
+    //labelQuoteStart = new JLabel(String.format("<html><div style=\"width:250px;\">\"%s\" %s</div><html>", KeynoteWriter.getQuote()));
+    labelQuoteStart = new JLabel(String.format("<html><div style=\"text-align:justify;text-justify:inter-word;width:250px;\">%s</div><html>", "\"Oh, please. That doesn\'t even sound like him! The President\'s an idiot, you don\'t sound like an idiot. If you don\'t wanna talk to your mother, just avoid me like usual, huh? I\'ll just throw myself in traffic. I\'ll just Saran Wrap myself to the bed and pretend my child is suffocating me... \" "));
     labelQuoteStart.setFont(new Font("Serif", Font.PLAIN, 15));
-    quotePanel.add(labelQuoteStart);
+    quotePanel.add(labelQuoteStart, BorderLayout.NORTH);
     
-    labelQuoteStart = new JLabel(String.format("(%s ", KeynoteWriter.getAuthor()));
-    //labelQuoteMiddle = new JLabel("(Author ");
+    JPanel citationPanel = new JPanel();
+    citationPanel.setLayout(new FlowLayout());
+    quotePanel.add(citationPanel, BorderLayout.CENTER);
+    
+    //labelQuoteStart = new JLabel(String.format("(%s ", KeynoteWriter.getAuthor()));
+    labelQuoteMiddle = new JLabel("(Korben's Mother ");
     labelQuoteMiddle.setFont(new Font("Serif", Font.PLAIN, 15));
-    quotePanel.add(labelQuoteMiddle);
+    citationPanel.add(labelQuoteMiddle);
     
-    inputPage = new JTextField(String.valueOf(KeynoteWriter.getPage()), 3);
-    //inputPage = new JTextField("0", 3);
+    //inputPage = new JTextField(String.valueOf(KeynoteWriter.getPage()), 3);
+    inputPage = new JTextField("96", 3);
     inputPage.setHorizontalAlignment(SwingConstants.CENTER);
-    quotePanel.add(inputPage);
+    citationPanel.add(inputPage);
     
     labelQuoteEnd = new JLabel(").");
     labelQuoteEnd.setFont(new Font("Serif",Font.PLAIN, 15));
-    quotePanel.add(labelQuoteEnd);
+    citationPanel.add(labelQuoteEnd);
     //END Quote Panel
     
     //START Buttons
     JButton nextAnalysisButton = new JButton("Re-Analyze");
     nextAnalysisButton.addActionListener(new NextListener());
     c.fill        = GridBagConstraints.HORIZONTAL;
+    c.anchor      = GridBagConstraints.NORTH;
     c.gridx       = 3;
     c.gridy       = 0;
     c.gridwidth   = 1;
@@ -58,6 +63,7 @@ public class KeynotePanel extends JPanel {
     JButton addQuoteButton = new JButton("Add");
     addQuoteButton.addActionListener(new AddListener());
     c.fill        = GridBagConstraints.HORIZONTAL;
+    c.anchor      = GridBagConstraints.NORTH;
     c.gridx       = 3;
     c.gridy       = 1;
     c.gridwidth   = 1;
@@ -68,6 +74,7 @@ public class KeynotePanel extends JPanel {
     JButton cancelQuoteButton = new JButton("Cancel");
     cancelQuoteButton.addActionListener(new CancelListener());
     c.fill        = GridBagConstraints.HORIZONTAL;
+    c.anchor      = GridBagConstraints.NORTH;
     c.gridx       = 3;
     c.gridy       = 2;
     c.gridwidth   = 1;
@@ -77,8 +84,8 @@ public class KeynotePanel extends JPanel {
     //END Buttons
     
     //START Analysis Box
-    inputAnalysis = new JTextArea(KeynoteWriter.getAnalysis());
-    //inputAnalysis = new JTextArea("An analysis");
+    //inputAnalysis = new JTextArea(KeynoteWriter.getAnalysis());
+    inputAnalysis = new JTextArea("An analysis");
     inputAnalysis.setLineWrap(true);
     inputAnalysis.setWrapStyleWord(true);
     c.fill        = GridBagConstraints.BOTH;
@@ -103,8 +110,8 @@ public class KeynotePanel extends JPanel {
       KeynoteWriter.save();
       KeynoteWriter.nextQuote();
       
-      labelQuoteStart.setText(String.format("\"%s\" ", KeynoteWriter.getQuote()));
-      inputPage.setText(KeynoteWriter.getPage());
+      labelQuoteStart.setText(String.format("<html><div style=\"width:250px;\">\"%s\" %s</div><html>", KeynoteWriter.getQuote()));
+      inputPage.setText(String.valueOf(KeynoteWriter.getPage()));
       inputAnalysis.setText(KeynoteWriter.getAnalysis());
     }
   }
@@ -114,7 +121,7 @@ public class KeynotePanel extends JPanel {
       KeynoteWriter.nextQuote();
       
       labelQuoteStart.setText(String.format("\"%s\" ", KeynoteWriter.getQuote()));
-      inputPage.setText(KeynoteWriter.getPage());
+      inputPage.setText(String.valueOf(KeynoteWriter.getPage()));
       inputAnalysis.setText(KeynoteWriter.getAnalysis());
     }
   }
